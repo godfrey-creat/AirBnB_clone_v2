5a337b549d7786f364eb1d72ad4e02b7522c60f6
rarimuk <arimukruth@gmail.com>
2023-06-23 07:01:46 -0700

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-"""__init__ method for models """
-
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
 
-
-
-
 storage = FileStorage()
-
 storage.reload()
--- models/state.pt
+++ models/state.pt
@@ -3,5 +3,6 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
+class State(BaseModel):
+    """ State class """
     name = ""
